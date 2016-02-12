3f399214254293462f8f079f26df0c4526a1cf15
Cynrick Chiu <cyn.2k5@outlook.com>
2016-02-12 09:52:44 -0800

    Edit spacing

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,20 +1,20 @@
 class LinksController < ApplicationController
   def index
-  	@links = Link.all
+    @links = Link.all
   end
 
   def create
-  	@link = Link.create(link_params)
+    @link = Link.create(link_params)
 
     if @link.valid?
       flash[:success] = 'New link successfully created.'
       redirect_to root_path
     end
 
-  	if @link.invalid?
-  	  flash[:error] = 'The data you entered is invalid. Please try again.'
-  	  redirect_to root_path
-  	end
+    if @link.invalid?
+      flash[:error] = 'The data you entered is invalid. Please try again.'
+      redirect_to root_path
+    end
   end
 
   private
